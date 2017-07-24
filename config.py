d1403956dacbd77445c4449cee72965101e56c19
Gioia Ballin <gballin@measurence.com>
2017-07-24 10:35:39 +0200

    Deleted port specification in config.py.

--- config.py
+++ config.py
@@ -10,29 +10,29 @@ class BaseConfig:
     TESTING = False
     # The database URI should be provided in the following format:
     # postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@host:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user@host:port/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user@localhost:port/dbname'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class TestingConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@testhost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user@testhost:port/dbname'
     TESTING = True
 
 
 class StagingConfig(BaseConfig):
     # TODO: set the db url and the other parameters for the StagingConfig
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@staginghost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user@staginghost:port/dbname'
 
 
 class ProductionConfig(BaseConfig):
     # TODO: set the db url and the other parameters for the ProductionConfig
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user@productionhost:5432/dbname'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user@productionhost:port/dbname'
 
 
 # Collects all the available configurations in a dictionary
